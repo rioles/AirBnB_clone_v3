f7e4751584c8c7e480c7da65c6c4d07cb1269fd5
KeeneKabalisa <keenekabalisa@gmail.com>
2022-10-23 22:50:30 +0200

    Fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ Starts a Flask web application
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
