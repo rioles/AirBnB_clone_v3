737d881ae58ba15a855e3aaae2f376c28ef2944a
rioles <rioles1992@gmail.com>
2022-10-24 17:31:13 +0100

    all stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns status of API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats',  methods=['GET'], strict_slashes=False)
+def stats():
+    """Returns status of all classes"""
+    all_type = {"amenities": "Amenity", "cities": "City", "places": "Place",
+                "reviews": "Review", "states": "State", "users": "User"}
+    stats_object_by_type = {}
+    for element in all_type:
+        stats_object_by_type[element] = storage.count(all_type[element])
+    stats_object_by_type = jsonify(stats_object_by_type)
+    stats_object_by_type.status = 200
+
+    return stats_object_by_type
