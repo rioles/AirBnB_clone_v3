bc313e1b71b23e7b8bd4709c03b0ec82c54583eb
rioles <rioles1992@gmail.com>
2022-10-24 15:13:12 +0100

    fix pep8 style

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,15 +11,18 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(self):
     """ Calls storage close"""
     storage.close()
 
+
 @app.errorhandler(404)
 def pageNotFound(error):
     """Error handling for 404"""
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
